ac11f484d30fdbae9a9e59cd22bb698be2565167
Youssef Sherif <sharief@aucegypt.edu>
2019-02-01 05:44:59 +0200

    Skip an api test

--- src/core/api-service.spec.js
+++ src/core/api-service.spec.js
@@ -2,7 +2,7 @@ const service = require('./api-service.js')
 const config = require('../config/config')
 
 // there might be a problem here
-describe('api service', () => {
+xdescribe('api service', () => {
     describe('request to AUC course offerings', () => {
         it('should return a number', async () => {
             const result = await service.getRemainingSeats(config.course.term, config.course.subject)
